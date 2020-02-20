7db2bbbdcc8afe8f12ab7e8d3814f70e658fd5ae
Fidel Thomet <me@fidelthomet.com>
2020-02-20 10:08:26 +0100

    [preprocessing] fix: keep scenario column

--- preprocessing/oil-risks.jl
+++ preprocessing/oil-risks.jl
@@ -1,23 +1,18 @@
 using DataFrames, CSV, Statistics, StatsBase
 
 df = CSV.read("./input/Risks Data - 1802 - Fossil Fuel.csv")
-select!(df, Not(:Scenario))
 select!(df, Not(:Units))
 
-df = stack(df, 5:size(df)[2])
-rename!(df, [:year, :value, :category, :variable, :model, :region])
+df = stack(df, 6:size(df)[2])
+rename!(df, [:year, :value, :category, :variable, :model, :scenario, :region])
 
 df = df[.|(df.year .== Symbol(2020), df.year .== Symbol(2040), df.year .== Symbol(2060), df.year .== Symbol(2080), df.year .== Symbol(2100)), :]
 
 df = unstack(df, :year, :value)
 
-
 df = df[.|(df.category .== "Direct emissions cost", df.category .== "Revenue"), :]
 df = df[.|(df.variable .== "Gas", df.variable .== "Coal", df.variable .== "Oil"), :]
 
-df.Model
-df[Symbol("Model")]
+# df[df.scenario .== "Primary Energy|Biomass",:variable] .= "Biomass Energy"
 
-df =
-
-df[Symbol("Risk Factor Pathway")]
+CSV.write("../src/assets/data/oil-risks.csv", df)
