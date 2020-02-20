3c268464540362ea4df8500f353aca2d00034a76
Fidel Thomet <me@fidelthomet.com>
2020-02-20 12:37:19 +0100

    [preprocessing] new data

--- preprocessing/oil-risks.jl
+++ preprocessing/oil-risks.jl
@@ -1,18 +1,19 @@
-using DataFrames, CSV, Statistics, StatsBase
+using DataFrames, CSV, Statistics, StatsBase, XLSX
 
-df = CSV.read("./input/Risks Data - 1802 - Fossil Fuel.csv")
-select!(df, Not(:Units))
+df = DataFrame(XLSX.readtable("./input/RiskFactorPathways_CD-LINKS.xlsx", "rfps_wide_cd-links_all")...)
+df.scenario = map(v -> v[1], split.(df.scenario, " "))
+df.scenario = replace.(df.scenario, "CD-LINKS_" => "")
+df.category = map(v -> v[2], split.(df.variable, "|"))
+df.var = map(v -> v[size(v)[1]], split.(df.variable, "|"))
+select!(df, Symbol.(["model", "scenario", "region", "var", "category", 2020, 2040, 2060, 2080, 2100]))
 
 df = stack(df, 6:size(df)[2])
-rename!(df, [:year, :value, :category, :variable, :model, :scenario, :region])
+rename!(df, [:year, :value, :model, :scenario, :region, :variable, :category])
 
-df = df[.|(df.year .== Symbol(2020), df.year .== Symbol(2040), df.year .== Symbol(2060), df.year .== Symbol(2080), df.year .== Symbol(2100)), :]
-
-df = unstack(df, :year, :value)
 
 df = df[.|(df.category .== "Direct emissions cost", df.category .== "Revenue"), :]
 df = df[.|(df.variable .== "Gas", df.variable .== "Coal", df.variable .== "Oil"), :]
-
-# df[df.scenario .== "Primary Energy|Biomass",:variable] .= "Biomass Energy"
+df = df[.|(df.scenario .== "NPi", df.scenario .== "NPi2020_1000", df.scenario .== "NPi2020_400"), :]
+# df = unstack(df, :year, :value)
 
 CSV.write("../src/assets/data/oil-risks.csv", df)
