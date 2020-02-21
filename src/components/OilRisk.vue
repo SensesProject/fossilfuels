8f361ab2183737112bac0462b660e7bb7c77abaa
Fidel Thomet <me@fidelthomet.com>
2020-02-21 11:38:32 +0100

    [Update] oil/price risks

--- src/components/OilRisk.vue
+++ src/components/OilRisk.vue
@@ -1,7 +1,39 @@
 <template>
   <div class="oil-risk">
-    <svg :width="innerWidth" :height="height"></svg>
-</div>
+    <div class="wrapper" :style="{width: `${innerWidth}px`, height: `${height}px`}">
+      <div class="key">
+        Key <br>
+        {{ svg.width }} x {{ svg.height }}
+      </div>
+      <div class="chart" v-resize:debounce.initial="onResize">
+        <svg>
+          <g :transform="`translate(${layout.margin.left} ${layout.margin.top})`">
+            <g class="axis">
+              <g class="x-axis">
+                <g v-for="(y, i) in years" :key="`y-${i}`" :transform="`translate(${layout.colWidth * (i + 0.5)} 0)`">
+                  <circle cy="-5" r="5"/>
+                  <line :y2="layout.height"/>
+                  <text :y="layout.height + layout.lineHeight">{{ y }}</text>
+                </g>
+              </g>
+              <g class="y-axis">
+                <g v-for="(s, i) in scenarios" :key="`s-${i}`" :transform="`translate(0 ${layout.rowHeight * (i + 0.5)})`">
+                  <line :x2="layout.width - layout.colWidth * 0.5"/>
+                  <text :x="-8" :y="4">{{ s }}</text>
+                </g>
+              </g>
+            </g>
+            <g class="boxes">
+              <g v-for="(r, ri) in rows" :key="`r-${ri}`">
+                <OilRiskBoxes v-for="(c, ci) in r" :key="`c-${ri}-${ci}`" :data="c" :size="layout.boxSize" :max="max"
+                  :transform="`translate(${layout.colWidth * (ci + 0.5)} ${layout.rowHeight * (ri + 0.5)})`"/>
+              </g>
+            </g>
+          </g>
+        </svg>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,9 +41,17 @@
 // import _ from 'lodash'
 
 import risks from 'dsv-loader!@/assets/data/oil-risks.csv' // eslint-disable-line import/no-webpack-loader-syntax
+import resize from 'vue-resize-directive'
+import OilRiskBoxes from '@/components/OilRiskBoxes.vue'
 
 export default {
   name: 'oil-risk',
+  directives: {
+    resize
+  },
+  components: {
+    OilRiskBoxes
+  },
   props: {
     step: {
       type: Number,
@@ -28,11 +68,18 @@ export default {
   },
   data () {
     return {
-      // PrEnQuantity,
-      margins: {
-        risks,
-        marginleft: 65,
-        marginright: 65
+      risks,
+      scenarios: [...new Set(risks.map(r => r.scenario))],
+      models: [...new Set(risks.map(r => r.model))],
+      years: [...new Set(risks.map(r => r.year))],
+      categories: [...new Set(risks.map(r => r.category))],
+      variables: [...new Set(risks.map(r => r.variable))],
+      regions: [...new Set(risks.map(r => r.region))],
+      model: 'REMIND-MAgPIE 1.7-3.0',
+      region: 'World',
+      svg: {
+        width: 320,
+        height: 320
       }
     }
   },
@@ -40,6 +87,64 @@ export default {
     innerWidth () {
       const { width } = this
       return Math.min(width, 1000)
+    },
+    rows () {
+      const { risks, scenarios, years, region, model, variables, categories } = this
+      const risks1 = risks.filter(r => r.region === region && r.model === model)
+      return scenarios.map(s => {
+        const risks2 = risks1.filter(r => r.scenario === s)
+        return years.map(y => {
+          const risks3 = risks2.filter(r => r.year === y)
+          const vars = {}
+          variables.forEach(v => {
+            vars[v] = {}
+            categories.forEach(c => {
+              vars[v][c] = +risks3.find(r => r.variable === v && r.category === c).value
+            })
+          })
+          return vars
+        })
+      })
+    },
+    layout () {
+      const { svg, scenarios, years } = this
+      const margin = {
+        top: 16,
+        right: 0,
+        bottom: 16,
+        left: 32
+      }
+      const height = svg.height - margin.top - margin.bottom
+      const width = svg.width - margin.left - margin.right
+      const rowHeight = height / scenarios.length
+      const colWidth = width / years.length
+      return {
+        margin,
+        height,
+        width,
+        rowHeight,
+        colWidth,
+        lineHeight: 16,
+        boxSize: Math.min(rowHeight, colWidth) - 16
+      }
+    },
+    max () {
+      const { rows } = this
+      return Math.max(...rows.map(r => {
+        return r.map(c => {
+          return Object.keys(c).map(k => {
+            return Object.keys(c[k]).map(v => c[k][v]).reduce((a, b) => a + b)
+          })
+        })
+      }).flat(2))
+    }
+  },
+  methods: {
+    onResize (el) {
+      const { svg } = this
+      const rect = el.querySelector('svg').getBoundingClientRect()
+      svg.width = rect.width
+      svg.height = rect.height
     }
   }
 }
@@ -47,14 +152,67 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-@import "library/src/style/variables.scss";
+@import "library/src/style/global.scss";
 
 .oil-risk {
   display: flex;
   align-items: center;
   flex-direction: column;
 
-  svg {
-    background: getColor(red, 100);
+  .wrapper {
+    position: relative;
+    display: flex;
+
+    $key-width: 128px;
+    .key {
+      height: 100%;
+      width: $key-width;
+      padding: $spacing / 2;
+      background: getColor(green, 100);
+    }
+    .chart {
+      padding: $spacing / 2;
+      width: calc(100% - #{$key-width});
+      height: 100%;
+      svg {
+        display: block;
+        width: 100%;
+        height: 100%;
+        overflow: visible;
+
+        .axis {
+          .x-axis {
+            line {
+              stroke: $color-black;
+            }
+            text {
+              text-anchor: middle;
+            }
+          }
+          .y-axis {
+            line {
+              stroke: $color-black;
+              stroke-dasharray: 3 5;
+            }
+            text {
+              text-anchor: end;
+            }
+          }
+        }
+      }
+    }
+    @include max-width($narrow) {
+      $key-height: 128px;
+      flex-direction: column-reverse;
+      .key {
+        height: $key-height;
+        width: 100%;
+      }
+      .chart {
+        width: 100%;
+        height: calc(100% - #{$key-height});
+      }
+    }
   }
-}</style>
+}
+</style>
---
+++ src/components/OilRiskBoxes.vue
@@ -0,0 +1,85 @@
+<template>
+  <g class="oil-risk-boxes">
+    <rect v-for="(r, i) in rects" :key="`r-${i}`" v-bind="r"/>
+  </g>
+</template>
+
+<script>
+import { scalePow } from 'd3'
+
+export default {
+  name: 'oil-risk-boxes',
+  props: {
+    data: {
+      type: Object,
+      default: null
+    },
+    size: {
+      type: Number,
+      default: 100
+    },
+    max: {
+      type: Number,
+      default: 100
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {
+    scale () {
+      const { max, size } = this
+      return scalePow()
+        .exponent(1)
+        .domain([0, max])
+        .range([0, size / 2])
+    },
+    rects () {
+      const { data, scale } = this
+      return ['Gas', 'Oil', 'Coal'].map((k1, i) => {
+        return Object.keys(data[k1]).map(k2 => {
+          const size = scale(k2 === 'Revenue' ? (data[k1][k2]) : (data[k1][k2] + data[k1]['Revenue']))
+          return {
+            class: [k1, { filled: k2 === 'Revenue' }],
+            transform: `rotate(${90 * i}) translate(8 8)`,
+            width: size,
+            height: size
+          }
+        })
+      }).flat()
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import "library/src/style/global.scss";
+
+.oil-risk-boxes {
+  rect {
+    fill: none;
+
+    &.Coal {
+      stroke: getColor(gray, 40);
+      &.filled {
+        fill: getColor(gray, 80);
+      }
+    }
+
+    &.Oil {
+      stroke: getColor(orange, 40);
+      &.filled {
+        fill: getColor(orange, 80);
+      }
+    }
+
+    &.Gas {
+      stroke: getColor(red, 40);
+      &.filled {
+        fill: getColor(red, 80);
+      }
+    }
+  }
+}
+</style>
