cd598dd03fb7772de20bda4f24732319bb18cf05
Fidel Thomet <me@fidelthomet.com>
2020-02-25 14:34:30 +0100

    [preprocessing]

--- preprocessing/uncertainty.jl
+++ preprocessing/uncertainty.jl
@@ -2,7 +2,7 @@ using DataFrames, CSV, Statistics, StatsBase, XLSX
 
 df = CSV.read("./input/Uncertainty Viz - Gas FF Module - Slopes-Data.csv")
 select!(df, Symbol.(["Model", "Scenario", "Region", "Variable", 2020, 2030, 2040, 2050, 2060, 2070, 2080, 2090]))
-
+df.Variable = map(v -> v[1], split.(df.Variable, " "))
 df = stack(df, 5:size(df)[2])
 rename!(df, [:year, :value, :model, :scenario, :region, :variable])
 
